155f73e4b286256bc7a35617833bf9923506331c
marycarrigan4 <mary4@rams.colostate.edu>
2020-12-19 20:47:28 -0700

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Jumbotron } from 'reactstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron> <h1> Mary Carrigan </h1> </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
